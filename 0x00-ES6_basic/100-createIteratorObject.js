7385752ac3dde4d06192aa71345bf841f9b59c25
Emmanuel Udeji <88405403+emmaudeji@users.noreply.github.com>
2022-06-15 19:27:04 +0100

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,6 @@
 export default function createIteratorObject(report) {
   const emps = [];
-  // eslint-disable-next-line
 	for (const dep of Object.keys(report.allEmployees)) {
-    // eslint-disable-next-line
 		for (const emp of report.allEmployees[dep]) {
       emps.push(emp);
     }
