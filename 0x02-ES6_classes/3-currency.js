c36ba557b3f8b82b65216726a3ef594855557f99
Emmanuel Udeji <88405403+emmaudeji@users.noreply.github.com>
2022-06-16 14:26:26 +0100

    getters and setters

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -10,9 +10,6 @@ export default class Currency {
   }
 
   set code(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
-    } 
     this._code = value;
   }
 
@@ -22,9 +19,6 @@ export default class Currency {
   }
 
   set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
     this._name = value;
   }
 
