32c0a2cf9810e69b3382b4bf03b29814eea9b4b0
Emmanuel Udeji <88405403+emmaudeji@users.noreply.github.com>
2022-06-21 19:11:33 +0100

    array and filter

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,5 @@
+export default function getStudentsByLocation(list, city) {
+  if (!Array.isArray(list) || typeof city !== 'string') 
+    return [];
+  return list.filter((value) => value.location === city);
+}
